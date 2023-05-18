b28be1ed62175b89f928692be1cee1c8401c164d
Christopher Debray <christopher.debray@club-employes.com>
2023-05-18 16:51:13 +0200

    ADD display of 10 elements in php / nginx

--- docker/php/index.php
+++ docker/php/index.php
@@ -1,3 +1,22 @@
 <?php
 
-echo "Hello";
+$host = 'database';
+$port = '5432';
+$dbname = 'esgi';
+$user = 'esgi';
+$password = 'Test1234';
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+$pdo = new PDO($dsn, $user, $password);
+
+$stmt = $pdo->prepare('SELECT * FROM esgi');
+$stmt->execute();
+$rows = $stmt->fetchAll();
+
+foreach ($rows as $row): ?>
+<ul>
+  <li>ID : <?= $row['id'] ?></li>
+  <li>Titre : <?= $row['titre'] ?></li>
+  <li>Done : <?= $row['done'] ?></li>
+</ul>
+<?php endforeach ?>
